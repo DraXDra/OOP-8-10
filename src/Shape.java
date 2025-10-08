289da10897304ebcae6f727c4c1812b786f6a5f4
Ánh Chu Đức <chuducanh240406@gmail.com>
2025-10-08 16:36:06 +0700

    Shape

---
+++ src/Shape.java
@@ -0,0 +1,26 @@
+public abstract class Shape {
+    protected Point centerPoint;
+    private static int shapeCount = 0;
+
+    public Shape(Point centerPoint) {
+        this.centerPoint = centerPoint;
+        shapeCount++;
+    }
+
+    public Point getCenterPoint() {
+        return centerPoint;
+    }
+
+    public void setCenterPoint(Point centerPoint) {
+        this.centerPoint = centerPoint;
+    }
+
+    public static int getShapeCount() {
+        return shapeCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape[centerPoint=" + centerPoint + "]";
+    }
+}
