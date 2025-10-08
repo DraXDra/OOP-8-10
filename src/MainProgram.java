f937c46c843a3dc75db3e666929c4a877af684df
Ánh Chu Đức <chuducanh240406@gmail.com>
2025-10-08 16:49:11 +0700

    test

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -4,7 +4,6 @@
  */
 public class MainProgram {
     public static void main(String[] args) {
-        // Tạo danh sách các hình
         Shape[] shapes = new Shape[4];
 
         shapes[0] = new Rectangle(new Point(2, 3), 4, 6);
@@ -12,16 +11,35 @@ public class MainProgram {
         shapes[2] = new Circle(new Point(0, 0), 2.5);
         shapes[3] = new Rectangle(new Point(7, 1), 2, 3);
 
-        // 1️⃣ Số lượng hình đã khởi tạo
+        /**
+         * Demonstration of functionalities:
+         * 1️⃣ Total number of shapes created.
+         * 2️⃣ Detailed list of all shapes.
+         * 3️⃣ List of center points of all shapes.
+         */
+
+        /**
+         * Total number of shapes created
+         * Using static method getShapeCount() from Shape class
+         * to display the total count of shapes.
+         */
         System.out.println("Số lượng hình đã khởi tạo: " + Shape.getShapeCount());
 
-        // 2️⃣ Danh sách chi tiết các hình
+        /**
+         * Detailed list of all shapes
+         * Using toString() method of each shape
+         * to display their details.
+         */
         System.out.println("\nDanh sách chi tiết các hình:");
         for (Shape s : shapes) {
             System.out.println(s);
         }
 
-        // 3️⃣ Danh sách tâm điểm
+        /**
+         * List of center points of all shapes
+         * Using getCenterPoint() method of each shape
+         * to display their center points.
+         */
         System.out.println("\nDanh sách tâm điểm:");
         for (Shape s : shapes) {
             System.out.println(s.getCenterPoint());
